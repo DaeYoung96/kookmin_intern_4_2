a7444b4eaaa0cd0ecb73a165edd6768fc9335901
Ubuntu <ubuntu@ip-172-31-24-125.ap-northeast-2.compute.internal>
2020-09-10 11:17:48 +0000

    insert

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ display.start()
 
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(1)
-
+wait = WebDriverWait(driver, 20)
 result = pd.DataFrame(columns=['username', 'phone', 'name', 'address', 'career', 'info_company', 'info_biz', 'key'])
-result.to_csv('result 01.csv', encoding='CP949')
-f = open('result 01.csv', 'w', encoding='CP949', newline='')
+result.to_csv('result 44.csv', encoding='CP949')
+f = open('result 44.csv', 'w', encoding='CP949', newline='')
 wr = csv.writer(f)
 wr.writerow(['username', 'phone', 'name', 'address', 'career', 'info_company', 'info_biz', 'key'])
 # 로그인
@@ -25,8 +25,10 @@ driver.find_element_by_name('user_id').send_keys('remigailard80')
 driver.find_element_by_id('input_password').send_keys('@bolt123')
 driver.find_element_by_class_name('btn-common').click()
 
+start_url = 'https://www.makervil.com/user/company/viewCompanyListMain.do?CompanySearch=&area=44'
+
 # 기업리스트 페이지로 이동
-driver.get('https://www.makervil.com/user/company/viewCompanyListMain.do?CompanySearch=&area=30')
+driver.get(start_url)
 # 6페이지부터 시작
 # driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[2]/ul/li[6]/a').click()
 
@@ -38,14 +40,21 @@ tmp = 2
 flag = False
 cnt = 0
 # 5318
-for i in range(0, 17, 1):
+for i in range(0, 74, 1):
     # 확인하기 위한 페이지 출력
     print("page : ", page)
     # 해당 페이지 기업 리스트 20개 탐색 시작
     for j in range(1, 21, 1):
-        driver.implicitly_wait(3)
+        time.sleep(0.5)
         list_xpath = '//*[@id="content"]/div[1]/div[1]/ul/li[' + str(j) + ']/div[2]'
+        wait.until(EC.presence_of_element_located((By.XPATH, list_xpath)))
         driver.find_element_by_xpath(list_xpath).click()
+
+        while True:
+            if driver.current_url == 'https://www.makervil.com/user/company/viewCompanyListMain.do':
+                driver.find_element_by_xpath(list_xpath).click()
+            else:
+                break
 
         # 확인하기 위한 파트너 번째 수
         print("list : ", j)
@@ -84,10 +93,18 @@ for i in range(0, 17, 1):
 
         # 뒤로가기 실행
         driver.back()
+        while True:
+            if driver.current_url == 'https://www.makervil.com/user/company/viewCompanyListMain.do' or driver.current_url == start_url:
+                break
+            else:
+                driver.back()
 
     # 20개의 리스트가 끝나면 다음 페이지로 이동
-    driver.find_element_by_xpath('//*[@id="content"]/div[1]/div[2]/ul/li['+ str(tmp) +']/a').click()
-    
+    time.sleep(0.3)
+    next_page_xpath = '//*[@id="content"]/div[1]/div[2]/ul/li['+ str(tmp) +']/a'
+    wait.until(EC.presence_of_element_located((By.XPATH, next_page_xpath)))
+    driver.find_element_by_xpath(next_page_xpath).click()
+
     if flag == False:
         tmp += 1
 
